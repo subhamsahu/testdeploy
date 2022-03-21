2aa85cc22c8a2350a1d6928e91d8995c8fa4fe7f
subhamsahu <sam766626@gmail.com>
2022-03-21 16:01:31 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,8 @@
 from django.urls import include, path
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -27,4 +29,6 @@ urlpatterns = [
     path('api/v1/', include('apps.urls')),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path(r'redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+print(urlpatterns)
